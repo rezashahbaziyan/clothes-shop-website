2e829f3eaf99c7dd0509a6d51cd885df5dc20f93
ujin <ujinn.js@gmail.com>
2023-02-13 16:51:00 +0330

    upload

---
+++ Persian/app.js
@@ -0,0 +1,11 @@
+var newsPhoneNumberButten = document.getElementsByClassName(".newsPhoneNumberButten");
+var telNumberInput = document.getElementsByClassName(".telNumberInput").value;
+var a ;
+var customerNumber  = [];
+
+function myfunction(){
+    customerNumber[a] = telNumberInput;
+    a++;
+    console.log(customerNumber);
+}
+
